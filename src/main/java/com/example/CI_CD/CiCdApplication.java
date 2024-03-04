34df5811ed09644cc917e5856e958403d1ae9865
deepak.lal <deepaklalfms@gmail.com>
2024-03-04 14:56:44 +0500

    origin

--- src/main/java/com/example/CI_CD/CiCdApplication.java
+++ src/main/java/com/example/CI_CD/CiCdApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CiCdApplication {
 
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String getMessage(){
 		return "Hello_World";
 	}
